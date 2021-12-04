ef4cc03232e049845d5d66cecd7146da1282665f
celtkb <celtkb@gmail.com>
2021-12-04 02:49:39 +0100

    Die Funktion um das Ende des Spiels anzuzeigen ist jetzt in der Anzeige-klasse.

--- src/Anzeige.java
+++ src/Anzeige.java
@@ -13,20 +13,28 @@ public class Anzeige {
             for (int j=0; j < 3; j++){
 
                 //Wenn das eine Zelle den Werte -1 hat, dann hat dort der Computer gespielt.
-                if (board[i][j]==-1){
+                if (board[i][j] == -1) {
                     System.out.print("O ");
                 }
                 // Für den Spieler hat man den Wert 1
-                else if (board[i][j]==1) {
+                else if (board[i][j] == 1) {
                     System.out.print("X ");
-                }
-                else{
+                } else {
                     System.out.print("- ");
                 }
             }
 
-            System.out.println(i+1);
+            System.out.println(i + 1);
         }
         System.out.println("**1*2*3**\n");
     }
+
+    public static void endeAnzeigen(TicTacToe ticTacToeGame) {
+        //Wenn das Spiel zu Ende ist, "Game Over" anzeigen und das Ergebnis anzeigen.
+        System.out.println("Game Over!!! ");
+        System.out.println(ticTacToeGame.spielErgebnis());
+        Anzeige.brettAnzeigen(ticTacToeGame.getBoard());
+    }
+
+
 }
--- src/Game.java
+++ src/Game.java
@@ -7,14 +7,14 @@ public class Game {
 
     public static void main(String[] args) {
 
-        TicTacToe ticTaToeGame = new TicTacToe();
+        TicTacToe ticTacToeGame = new TicTacToe();
 
         int xInput;
         int yInput;
 
         while (true) {
             //Das Brett wird jedes Mal angezeigt. Am Anfang ist das Brett leer.
-            Anzeige.brettAnzeigen(ticTaToeGame.getBoard());
+            Anzeige.brettAnzeigen(ticTacToeGame.getBoard());
             System.out.println("Sie sind dran");
 
 
@@ -23,26 +23,22 @@ public class Game {
             yInput = SpielerEingabe.eingabeLesen("Y");
 
             //Hier spielt der Spieler und die Funktion gibt 1 als Wert zurück, wenn alles ohne Probleme geklappt hat
-            int playerStep = ticTaToeGame.spielerZug(yInput - 1, xInput - 1);
+            int playerStep = ticTacToeGame.spielerZug(yInput - 1, xInput - 1);
 
             //Die Funktion "spielrZug" liefert 1, wenn der Spieler dran ist und -1, wenn der Computer dran ist
             if (playerStep == 1) {
                 //Da der Spieler gespielt hat, und es geklappt hat, wird geprüft, ob das Spiel schon zu Ende ist
-                if (ticTaToeGame.spielende()) {
+                if (ticTacToeGame.spielende()) {
                     //Wenn das Spiel zu Ende ist, "Game Over" anzeigen und das Ergebnis anzeigen.
-                    System.out.println("Game Over!!! ");
-                    System.out.println(ticTaToeGame.spielErgebnis());
-                    Anzeige.brettAnzeigen(ticTaToeGame.getBoard());
+                    Anzeige.endeAnzeigen(ticTacToeGame);
                     //Die While-Schleife wird abgebrochen, wenn das Spiel zu Ende ist.
                     break;
                 }
                 //Wenn der Spiele gespielt hat und alles geklappt hat, dann ist der Computer dran.
-                ticTaToeGame.computerZug();
+                ticTacToeGame.computerZug();
                 //Nachdem der Computer gespielt hat, wird geprüft, ob das Spiel zu Ende ist.
-                if (ticTaToeGame.spielende()) {
-                    System.out.println("Game Over!!! ");
-                    System.out.println(ticTaToeGame.spielErgebnis());
-                    Anzeige.brettAnzeigen(ticTaToeGame.getBoard());
+                if (ticTacToeGame.spielende()) {
+                    Anzeige.endeAnzeigen(ticTacToeGame);
                     break;
                 }
             }
